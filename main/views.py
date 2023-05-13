9d3dc34a61f47c22c2c4743c324f93ad33659d43
ycrodriguez <yohancruzrodriguez@gmail.com>
2023-05-13 15:31:25 +0200

    login register form finish

--- main/views.py
+++ main/views.py
@@ -1,11 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, CreateView
 from django.contrib.auth import authenticate, login, logout
 
 from main.forms import UserForm
 
 
-class Login(ListView, DetailView):
+class Login(ListView, CreateView):
     def get(self, request, *args, **kwargs):
         form = UserForm()
         context = {'form': form}
@@ -25,21 +26,30 @@ class Login(ListView, DetailView):
         return render(request, 'custom/login.html', context)
 
 
-class Register(ListView, DetailView):
+class Register(ListView, CreateView):
     def get(self, request, *args, **kwargs):
-        return render(request, 'custom/register.html')
+        formRegisterUser = UserCreationForm()
+        context = {'form': formRegisterUser}
+        return render(request, 'custom/register.html', context)
+
+    def post(self, request, *args, **kwargs):
+        formRegisterUser = UserCreationForm(request.POST)
+        if formRegisterUser.is_valid():
+            formRegisterUser.save()
+            return redirect('login')
+        return render(request, 'custom/register.html', {'form': formRegisterUser})
 
 
-class Panel(ListView, DetailView):
+class Panel(ListView, CreateView):
     def get(self, request, *args, **kwargs):
         return render(request, 'custom/panel.html')
 
 
-class Productos(ListView, DetailView):
+class Productos(ListView, CreateView):
     def get(self, request, *args, **kwargs):
         return render(request, 'custom/productos.html')
 
 
-class Prefactura(ListView, DetailView):
+class Prefactura(ListView, CreateView):
     def get(self, request, *args, **kwargs):
         return render(request, 'custom/prefacturas.html')
